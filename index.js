4769f6acea3a9303bb50ef144c94cdc954c88d0c
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-05 01:21:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 function mapToNegativize(sourceArray){
   return sourceArray
 }
+function mapToNoChange(sourceArray){
+  return sourceArray
+}
+function mapToDouble(sourceArray){
+  return sourceArray
+}
+function mapToSquare(sourceArray){
+  return sourceArray
+}
